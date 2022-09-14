5661789577b2ca509321dcdbb29ed8f2dbbe8edd
Murt15 <murtazat941@gmail.com>
2022-09-14 07:34:33 +0530

    html task 3

--- demo.js
+++ demo.js
@@ -1,1 +1,6 @@
-console.log('I  dont love git');
+var header=document.getElementById('main-header');
+header.style.border='solid 5px #000';
+var items=document.getElementById(''); 
+console.log(items);
+items.style.fontWeight='bold';
+items.style.backgroundColor='Yellow';
