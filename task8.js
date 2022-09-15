70049078026653c1896b8cd35115fb6d2dfd33ed
Murt15 <murtazat941@gmail.com>
2022-09-15 07:54:18 +0530

    task8

---
+++ task8.js
@@ -0,0 +1,58 @@
+var form = document.getElementById('addForm');
+
+var itemList = document.getElementById('items');
+
+var filter = document.getElementById('filter');
+
+form.addEventListener('submit', addItem);
+
+itemList.addEventListener('click', removeItem);
+
+filter.addEventListener('keyup', filterItems);
+
+function addItem(e)
+{
+ e.preventDefault();
+  var newItem = document.getElementById('item').value;
+
+  var li = document.createElement('li');
+
+  li.className = 'list-group-item';
+
+  li.appendChild(document.createTextNode(newItem));
+
+  var deleteBtn = document.createElement('button');
+
+  deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+
+  deleteBtn.appendChild(document.createTextNode('X'));
+
+  li.appendChild(deleteBtn);
+
+  itemList.appendChild(li);
+
+  var submitBtn=document.createElement('button');
+
+  submitBtn.className='submit';
+
+  submitBtn.style.marginLeft='800px';
+
+  submitBtn.appendChild(document.createTextNode('Submit'));
+
+  li.appendChild(submitBtn);
+
+  itemList.appendChild(li);
+}
+
+function removeItem(e)
+{
+  if(e.target.classList.contains('delete'))
+  {
+    if(confirm('Confirm?'))
+    {
+      var li = e.target.parentElement;
+
+      itemList.removeChild(li);
+    }
+  }
+}
